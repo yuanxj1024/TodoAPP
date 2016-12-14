55c1110628b067264d77abd38b13342009cde603
Aaron-vk-Yuan <xuanyuanziruo@gmail.com>
2016-12-14 09:10:33 +0800

    main tab view

--- src/containers/MainTabsView.js
+++ src/containers/MainTabsView.js
@@ -1,13 +1,58 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
-import {
-  StyleSheet,
-  View,
-  Text,
-  Image,
-  Dimensions,
-} from 'react-native';
+import {StyleSheet, View, Text, Image, Dimensions} from 'react-native';
 import HomeVew from './HomeView';
 import {Main} from '../components/main-tabs-view';
 import Actions from '../actions';
+
+class MainTabsView extends Component {
+  renderTab = (idx) => {
+    cosnt {navigator} = this.props;
+    switch (idx) {
+      case 0:
+        return <HomeView navigator={navigator}/>;
+        break;
+      case 1:
+        return (
+          <View>
+            <Text>Completed</Text>
+          </View>
+        );
+      case 2:
+        return (
+          <View>
+            <Text>Completed</Text>
+          </View>
+        );
+      default:
+        return null;
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Main {...this.props} renderTab={this.renderTab}/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
+
+function mapStateToProps(state) {
+  return {tab: state.navigator.index};
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(Actions, dispatch)
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MainTabsView);
